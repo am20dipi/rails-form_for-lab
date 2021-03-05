498dd5028ed4c52e38de98c3d452b02dd1e8c48a
Adriana <am20dipi@alum.siena.edu>
2021-03-05 14:34:50 -0500

    commit tree 62238bdf07c7b64e6eb198914999e43fd212a25c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :school_classes, except: [:destroy, :index]
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+
+ #get '/school_classes/new', to: 'school_classes#new'
+  #get '/school_classes', to: 'school_classes#index'
+  #get '/school_class/:id', to: 'school_class#show'
+  #get '/school_classes/:id/edit', to: 'school_classes#edit'
+
 end
